4e0b05b8891d2e5622ddd1a10cc4971bee645c30
Milindu Chaman <nmilinduchaman@gmail.com>
2024-04-07 00:23:42 +0530

    Update LocationForm.js

--- frontend/client/src/components/LocationForm.js
+++ frontend/client/src/components/LocationForm.js
@@ -7,6 +7,7 @@ function LocationForm() {
   const [address, setAddress] = useState('');
   const [phone, setPhone] = useState('');
   const [serialNumber, setSerialNumber] = useState('');
+  const [image, setImage] = useState(null);
   const [type, setType] = useState('');
   const [status, setStatus] = useState('');
 
@@ -21,9 +22,10 @@ function LocationForm() {
 
   const handleDeviceSubmit = async (e) => {
     e.preventDefault();
-    const deviceData = { serialNumber, type, status };
+    const deviceData = { serialNumber, image, type, status };
     await ApiService.createDevice(deviceData);
     setSerialNumber('');
+    setImage(null);
     setType('');
     setStatus('');
   };
@@ -33,15 +35,21 @@ function LocationForm() {
     await handleDeviceSubmit(e);
   };
 
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    setImage(file);
+  };
+
   return (
     <form onSubmit={handleSubmit}>
       <label>Name: </label> <input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} /><br></br>
       <label>Address: </label> <input type="text" placeholder="Address" value={address} onChange={(e) => setAddress(e.target.value)} /><br></br>
       <label>Phone: </label> <input type="text" placeholder="Phone" value={phone} onChange={(e) => setPhone(e.target.value)} /><br></br>
       <label>Serial Number: </label><input type="text" placeholder="Serial Number" value={serialNumber} onChange={(e) => setSerialNumber(e.target.value)} /><br></br>
+      <label>Image: </label><input type="file" onChange={handleImageChange} /><br />
       <label>Type: </label><input type="text" placeholder="Type" value={type} onChange={(e) => setType(e.target.value)} /><br></br>
       <label>Status: </label><input type="text" placeholder="Status" value={status} onChange={(e) => setStatus(e.target.value)} /><br></br>
-      <button type="submit">Add Location</button>
+      <button type="submit">Submit</button>
     </form>
   );
 }
