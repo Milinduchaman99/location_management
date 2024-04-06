6aa6456e1085a5b74b61ed8fc084f746b878e99a
Milindu Chaman <nmilinduchaman@gmail.com>
2024-04-06 10:45:09 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors());
 
 // MongoDB connection
-mongoose.connect('mongodb://localhost:27017/location_management_db', {
+mongoose.connect('mongodb://127.0.0.1:27017/location_management_db', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
