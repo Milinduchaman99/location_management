db17ead484c4ca76f5779465d08b8dec7d8bf513
Milindu Chaman <nmilinduchaman@gmail.com>
2024-04-06 10:45:38 +0530

    Update deviceRoutes.js

---
+++ backend/routes/deviceRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+// Define your routes here
+// Example:
+router.get('/', (req, res) => {
+  res.send('Device routes');
+});
+
+module.exports = router;
