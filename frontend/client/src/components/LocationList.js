5c204b8c820648be8c78c6a4788a352019d21dfe
Milindu Chaman <nmilinduchaman@gmail.com>
2024-04-07 16:01:40 +0530

    Update LocationList.js

--- frontend/client/src/components/LocationList.js
+++ frontend/client/src/components/LocationList.js
@@ -15,9 +15,8 @@ function LocationList() {
   const updateLocation = async (locationId, updatedData) => {
     try {
       await ApiService.updateLocation(locationId, updatedData); // Use the API service to update the location
-      // Optionally, you can refresh the list after updating
-      const response = await ApiService.getLocations();
-      setLocations(response.data);
+      // Open a new tab to get the updated data
+      window.open(window.location.href, '_blank');
     } catch (error) {
       console.error('Error updating location:', error);
     }
@@ -27,7 +26,7 @@ function LocationList() {
   const deleteLocation = async (locationId) => {
     try {
       await ApiService.deleteLocation(locationId); // Use the API service to delete the location
-      // Optionally, you can refresh the list after deleting
+      // Refresh the list after deleting
       const response = await ApiService.getLocations();
       setLocations(response.data);
     } catch (error) {
@@ -42,7 +41,7 @@ function LocationList() {
         {locations.map(location => (
           <li key={location._id}>
             {location.name} - {location.address} - {location.phone}
-            <button onClick={() => updateLocation(location._id, { name: 'New Name', address: 'New Address', phone: 'New Phone' })}>Update</button>
+            <button onClick={() => updateLocation(location._id, {})}>Update</button>
             <button onClick={() => deleteLocation(location._id)}>Delete</button>
           </li>
         ))}
@@ -52,3 +51,4 @@ function LocationList() {
 }
 
 export default LocationList;
+
