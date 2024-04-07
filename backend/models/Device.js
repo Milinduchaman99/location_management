e4be641fa743a8c8211bf2bd98f5927f79f700ae
Milindu Chaman <nmilinduchaman@gmail.com>
2024-04-07 15:28:42 +0530

    Update Device.js

--- backend/models/Device.js
+++ backend/models/Device.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const deviceSchema = new mongoose.Schema({
     serialNumber: { type: String, required: true, unique: true },
     type: { type: String, enum: ['pos', 'kiosk', 'signage'], required: true },
-    image: { type: String }, // Assuming image is stored as URL
+    image: { data: Buffer, contentType: String }, // Store image as Buffer and its content type
     status: { type: String, enum: ['active', 'inactive'], required: true },
-  });
+});
 
-  module.exports = mongoose.model('Device', deviceSchema);
+module.exports = mongoose.model('Device', deviceSchema);
